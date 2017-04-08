d86cba19ae38f6fe164c3a941119ee7642fa5a3c
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2017-04-08 07:30:37 +0200

    EcmaScript: Introduce codegen ReturnStatement

---
+++ src/codegen/EcmaScript/generator/ReturnStatementCodeGenerator.ts
@@ -0,0 +1,15 @@
+import BaseGenerator from '../BaseGenerator'
+import { register } from '../factory'
+import { ReturnStatement } from '@/compiler/ast'
+
+@register(node => node instanceof ReturnStatement ? Infinity : 0)
+export class ReturnStatementCodeGenerator extends BaseGenerator<ReturnStatement> {
+	/**
+	 * Generates code for a given syntax tree.
+	 * This method is automatically called by the `BaseGenerator` class whenever necessary.
+	 * @param ast The syntax tree to generate code for.
+	 */
+	protected generateCodeConcrete(astNode: ReturnStatement) {
+		return `return ${astNode.returnValue.content}`
+	}
+}

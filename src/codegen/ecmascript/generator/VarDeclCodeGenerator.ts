083a791ae2ec32154dd18f85c34735a3e58de72b
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2017-04-08 11:07:54 +0200

    EcmaScript: Introduce `VarDeclCodeGenerator`

---
+++ src/codegen/ecmascript/generator/VarDeclCodeGenerator.ts
@@ -0,0 +1,28 @@
+import BaseGenerator from '../BaseGenerator'
+import { register, createForAstNode } from '../factory'
+import { VarDecl } from '@/compiler/ast'
+
+@register(node => node instanceof VarDecl ? 1 : 0)
+export class VarDeclCodeGenerator extends BaseGenerator<VarDecl> {
+	/**
+	 * Generates code for a given syntax tree.
+	 * This method is automatically called by the `BaseGenerator` class whenever necessary.
+	 * @param ast The syntax tree to generate code for.
+	 */
+	protected generateCodeConcrete(astNode: VarDecl) {
+		let assignmentCode = ''
+		if (typeof astNode.assignment.content !== 'undefined') {
+			assignmentCode = ` = ${createForAstNode(astNode.assignment)}`
+		}
+		return `${this.generateDeclarationKeyword(astNode)}${astNode.name.rawValue}${assignmentCode};\n`
+	}
+
+
+	/**
+	 * Generate the keyword part of the variable declaration, for example `var `.
+	 * @param ast The syntax tree to generate code for.
+	 */
+	protected generateDeclarationKeyword(astNode: VarDecl) {
+		return 'var '
+	}
+}

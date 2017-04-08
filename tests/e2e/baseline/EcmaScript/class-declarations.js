fddcb7b232aa961f0d960a47ee892c21d85d9a93
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2017-04-08 10:46:18 +0200

    E2E: Add tests for class declarations

---
+++ tests/e2e/baseline/EcmaScript/class-declarations.js
@@ -0,0 +1,21 @@
+/** @class Alpha */
+var Alpha = (function() {
+	/** @constructor */
+	function Alpha() {
+	}
+
+	return Alpha;
+})();
+
+
+/** @class Beta */
+var Beta = (function() {
+	/** @constructor */
+	function Beta() {
+		this.foo;
+		this.bar;
+	}
+
+	return Beta;
+})();
+

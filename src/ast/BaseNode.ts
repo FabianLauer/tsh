bc3b0e6db80f56a1605a957bbc2e8ba0fa2643d1
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2017-04-08 11:04:06 +0200

    AST: BaseNode: Introduce AST node `parent`s

--- src/ast/BaseNode.ts
+++ src/ast/BaseNode.ts
@@ -2,7 +2,29 @@
  * Base AST node.
  */
 export abstract class BaseNode {
+	// tslint:disable-next-line:variable-name
+	private _parent: BaseNode
 
+	public get parent() {
+		return this._parent
+	}
+
+	/**
+	 * Changes the parent of an AST node.
+	 * @param parent The new parent node.
+	 */
+	protected setParent(parent: BaseNode) {
+		this._parent = parent
+	}
+
+	/**
+	 * Changes the parent of an AST node.
+	 * @param node The node to set a new parent on.
+	 * @param parent The new parent to set on `node`.
+	 */
+	protected setParentOf(node: BaseNode, parent: BaseNode) {
+		node.setParent(parent)
+	}
 }
 
 export default BaseNode

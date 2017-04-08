bdee7d5e0bd433817be2c4564bca4e183c2650b9
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2017-04-08 08:02:37 +0200

    Jakefile: Add `build:clean` and `build:compiler`

--- tools/Jakefile.ts
+++ tools/Jakefile.ts
@@ -6,9 +6,21 @@ import { sh, jake } from './buildUtils'
 
 namespace('build', function () {
 
+	desc('Removes the built compiler.')
+	task('clean', () => {
+		sh `rm -rf ./build/`
+	})
+
 	desc('Compiles the parser.')
 	task('parser', () => {
 		sh `jison -o ./src/compiler/generatedParser.js ./src/compiler/grammar.bison ./src/compiler/grammar.lex`
+	})
+
+
+	desc('Compiles the compiler except for the parser.')
+	task('compiler', () => {
+		jake('build', 'clean')
+		sh `tsc`
 	})
 
 })
@@ -43,17 +55,16 @@ namespace('dev', function () {
 
 	desc('Compiles the parser, then runs tests for it. Useful when working on the grammar definitions.')
 	task('parser', () => {
-		sh `rm -rf ./build/`
+		jake('build', 'clean')
 		jake('build', 'parser')
-		sh `tsc`
+		jake('build', 'compiler')
 		sh `mocha build/tests/all.js --grep Parser`
 	})
 
 
 	desc('Compiles the compiler, then runs end to end tests. Useful when working on code generators.')
 	task('codegen', () => {
-		sh `rm -rf ./build/`
-		sh `tsc`
+		jake('build', 'compiler')
 		sh `mocha build/tests/all.js --grep E2E`
 	})
 

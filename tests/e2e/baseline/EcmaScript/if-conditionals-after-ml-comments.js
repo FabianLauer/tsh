0fb81a6c9202b6d39d7db2dfe6cf48872614ba77
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2017-04-09 12:36:57 +0200

    E2E: Add test cases for conditional statements after comments

---
+++ tests/e2e/baseline/EcmaScript/if-conditionals-after-ml-comments.js
@@ -0,0 +1,47 @@
+function a() {
+	if (cond) {
+	}
+}
+
+function b() {
+	if (cond) {
+	}
+	else {
+	}
+}
+
+function c() {
+	if (cond) {
+	}
+	else if (cond) {
+	}
+}
+
+function d() {
+	if (cond) {
+	}
+	else if (cond) {
+	}
+	else if (cond) {
+	}
+}
+
+function e() {
+	if (cond) {
+	}
+	else if (cond) {
+	}
+	else {
+	}
+}
+
+function f() {
+	if (cond) {
+	}
+	else if (cond) {
+	}
+	else if (cond) {
+	}
+	else {
+	}
+}

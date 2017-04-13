a87f0c61b5f55922018d845bb4f559d6d165a9af
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2017-04-13 16:28:14 +0200

    EcmaScript: Use new `utils/FactoryRegistry` as a code generator factory

--- src/codegen/ecmascript/factory.ts
+++ src/codegen/ecmascript/factory.ts
@@ -3,49 +3,16 @@
 /// Functions to register and instantiate EcmaScript code generators.
 ///
 
-/// Imports
-
 import { BaseNode } from '@/compiler/ast'
 import { ICodeGenerator } from '@/compiler/codegen/base'
-
-
-/// Types & Interfaces
-
-
-/**
- * Describes a type that constructs an `ICodeGenerator` when calling `new(BaseNode)`
- * on it.
- */
-export type IGeneratorConstructor<TNode extends BaseNode> = {
-	new (astNode: TNode): ICodeGenerator<TNode>
-}
-
+import { FactoryRegistry } from '../../utils/FactoryRegistry'
 
 
 /**
- * Describes rating function signatures used by the `register(...)` decorator.
+ * The factory registry used to instantiate code generators.
+ * @internal
  */
-export type IRatingFunc<TNode extends BaseNode> = (input: TNode) => number | void
-
-
-
-/**
- * Describes the objects used to store a code generator constructor in the `registry`.
- */
-interface IRegisteredGeneratorConstructor<TNode extends BaseNode> {
-	readonly ratingFunc: IRatingFunc<TNode>
-	readonly generatorType: IGeneratorConstructor<TNode>
-}
-
-
-/// Factory Implementation
-
-
-/**
- * Contains all registered code generators and their rating functions.
- */
-const registry: Array<IRegisteredGeneratorConstructor<BaseNode>> = []
-
+const factory = FactoryRegistry.create<BaseNode, ICodeGenerator<BaseNode>>()
 
 
 /**
@@ -55,59 +22,7 @@ const registry: Array<IRegisteredGeneratorConstructor<BaseNode>> = []
  *                   that this function returns, the more likely the code
  *                   generator is to be used for a given AST node.
  */
-export function register<TNode extends BaseNode>(ratingFunc: IRatingFunc<TNode>) {
-	return (generatorType: IGeneratorConstructor<TNode>) => {
-		registry.push({ ratingFunc, generatorType })
-	}
-}
-
-
-
-/**
- * Creates a function that can be used to sort arrays of `IRegisteredGeneratorConstructor`s
- * by their rating for a given AST node.
- * The function is ment to be passed into `Array.prototype.sort()`.
- * @param astNode The AST node to calculate ratings for.
- */
-function createRatingSorter<TNode extends BaseNode>(astNode: TNode) {
-	return (
-		a: IRegisteredGeneratorConstructor<TNode>,
-		b: IRegisteredGeneratorConstructor<TNode>
-	) => {
-		const orderA = a.ratingFunc(astNode)
-		const orderB = b.ratingFunc(astNode)
-		if (orderA > orderB) {
-			return -1
-		} else if (orderA < orderB) {
-			return 1
-		} else {
-			return 0
-		}
-	}
-}
-
-
-
-/**
- * Finds the best matching code generator for the given AST node and returns its
- * constructor.
- * @throws
- * @param astNode The AST node to get a matching a code generator constructor for.
- * @return The code generator constructor for the given AST node.
- */
-function findForAstNode<TNode extends BaseNode>(astNode: TNode): IGeneratorConstructor<TNode> {
-	const list = registry
-		// find all constructors that have a rating > 0
-		.filter(registered => (registered.ratingFunc(astNode) || 0) > 0)
-		// sort by rating descending
-		.sort(createRatingSorter(astNode))
-	if (list.length < 1) {
-		return undefined
-	}
-	// the first item in `list` is the item with the best match
-	return <IGeneratorConstructor<TNode>>list[0].generatorType
-}
-
+export const register: typeof factory.registerClass = factory.registerClass.bind(factory)
 
 
 /**
@@ -118,13 +33,4 @@ function findForAstNode<TNode extends BaseNode>(astNode: TNode): IGeneratorConst
  * @param astNode The AST node to instantiate a code generator for.
  * @return The code generator created for the given AST node.
  */
-export function createForAstNode<TNode extends BaseNode>(astNode: TNode) {
-	const constructor = findForAstNode(astNode)
-	// double check that a `constructor` was found, throw an error if not
-	if (typeof constructor !== 'function') {
-		throw new Error(
-			`Can not create code generator for AST node ${astNode.constructor.name}: no generator in registry.`
-		)
-	}
-	return <ICodeGenerator<TNode>>(new constructor(astNode))
-}
+export const createForAstNode: typeof factory.create = factory.create.bind(factory)

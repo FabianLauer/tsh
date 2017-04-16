045f98d33f9e0f4c6f4850f599af0885ac21c42a
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2017-04-16 23:44:38 +0200

    EcmaScript: Implement an export statement code generator

---
+++ src/codegen/ecmascript/generator/ExportStatementCodeGenerator.ts
@@ -0,0 +1,16 @@
+import BaseGenerator from '../BaseGenerator'
+import { register } from '../factory'
+import { ExportStatement } from '@/compiler/ast'
+
+@register(node => node instanceof ExportStatement ? Infinity : 0)
+export class ExportStatementCodeGenerator extends BaseGenerator<ExportStatement> {
+	/**
+	 * Generates code for a given syntax tree.
+	 * This method is automatically called by the `BaseGenerator` class whenever necessary.
+	 * @param ast The syntax tree to generate code for.
+	 */
+	protected generateCodeConcrete(astNode: ExportStatement) {
+		const exportedName = astNode.exportedIdentifier.name.rawValue
+		return `exports['${exportedName}'] = ${exportedName};\n`
+	}
+}

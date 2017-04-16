301a83d854274cffcb8518572f85ed79b961acd4
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2017-04-16 23:16:07 +0200

    E2E: Add import statement test

---
+++ tests/e2e/baseline/EcmaScript/import-statements.js
@@ -0,0 +1,14 @@
+var __import = require('./alpha');
+for (var key in __import) global[key] = __import[key];
+
+var __import = require('./beta');
+for (var key in __import) global[key] = __import[key];
+
+var __import = require('./gamma');
+for (var key in __import) global[key] = __import[key];
+
+function main() {
+}
+
+var __import = require('../../../../delta/./../someFile.123.abc');
+for (var key in __import) global[key] = __import[key];

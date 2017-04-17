ad4aebb74cd0ed77046308cb7f389df6eaeed4c1
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2017-04-17 11:02:16 +0200

    E2E: Add test for anonymous func delcs & expressions

---
+++ tests/e2e/baseline/EcmaScript/anon-func-decls.js
@@ -0,0 +1,62 @@
+function main() {
+	(function () {
+	});
+	(function () {
+	});
+
+
+	(function (a) {
+	});
+	(function (a) {
+	});
+	(function (a) {
+	});
+	(function (a) {
+	});
+
+
+	(function (a, b) {
+	});
+	(function (a, b) {
+	});
+	(function (a, b) {
+	});
+	(function (a, b) {
+	});
+	(function (a, b) {
+	});
+	(function (a, b) {
+	});
+
+
+	(function () {
+	});
+	((function (a, b, c) {
+		return a;
+	}));
+
+
+	(function () {
+		return (function () {
+		});
+	});
+	((function () {
+		return (function () {
+		});
+	}));
+
+	var a = (function () {
+		return (function () {
+		});
+	});
+
+
+	abc((function () {
+	}));
+	abc(((function () {
+	})));
+	abc((function () {
+		return (function () {
+		});
+	}));
+}

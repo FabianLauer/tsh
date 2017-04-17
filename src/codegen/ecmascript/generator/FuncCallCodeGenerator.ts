202804939c87c58dfa422c52519ba3556c09759f
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2017-04-17 10:15:27 +0200

    EcmaScript: Add code generator for function calls

---
+++ src/codegen/ecmascript/generator/FuncCallCodeGenerator.ts
@@ -0,0 +1,18 @@
+import BaseGenerator from '../BaseGenerator'
+import { register, createForAstNode } from '../factory'
+import { FuncCall } from '@/compiler/ast'
+
+@register(node => node instanceof FuncCall ? Infinity : 0)
+export class ExprListCodeGenerator extends BaseGenerator<FuncCall> {
+	/**
+	 * Generates code for a given syntax tree.
+	 * This method is automatically called by the `BaseGenerator` class whenever necessary.
+	 * @param ast The syntax tree to generate code for.
+	 */
+	protected generateCodeConcrete(astNode: FuncCall) {
+		return [
+			createForAstNode(astNode.identifier),
+			'(', createForAstNode(astNode.parameterList), ')'
+		]
+	}
+}

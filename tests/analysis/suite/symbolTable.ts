741751bd518bfcfc79bdc7a909495f4ebfd1ec79
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2017-04-22 20:50:11 +0200

    Analysis Tests: Add Tests for SymbolTable methods

--- tests/analysis/suite/symbolTable.ts
+++ tests/analysis/suite/symbolTable.ts
@@ -15,6 +15,7 @@ import * as ast from '@/compiler/ast'
  */
 export interface ITestSuiteDependencies {
 	getSymbolTableFor(node: IScope): SymbolTable
+	parseSourceCodeToAST(sourceCode: string): ast.SourceUnit
 }
 
 
@@ -23,6 +24,27 @@ export interface ITestSuiteDependencies {
  * @param dependencies The dependencies to inject into this test suite.
  */
 function runTestSuite(dependencies: ITestSuiteDependencies) {
+	///
+	/// Utilities:
+	///
+
+	function wrapAndParse(sourceCode: string): ast.FuncDecl {
+		const sourceUnit = dependencies.parseSourceCodeToAST(`
+			func __wrapper__() {
+				${sourceCode}
+			}
+		`)
+
+		return <ast.FuncDecl>sourceUnit.getNodeAtIndex(0)
+	}
+
+
+
+	///
+	/// Unit Tests:
+	///
+
+
 	// We only test a few nodes here to see if types are actually checked by the
 	// symbol table get/create logic. We can't really test *every* non-scope AST
 	// node type anyway since there will likely be many more non-scope AST node
@@ -189,6 +211,226 @@ function runTestSuite(dependencies: ITestSuiteDependencies) {
 		const decl = new ast.ElseStatement([])
 		const symbolTable = dependencies.getSymbolTableFor(decl)
 		assert(symbolTable instanceof SymbolTable)
+	})
+
+
+
+	///
+	/// `SymbolTable` Method Tests
+	///
+
+	it('`let` decl', () => {
+		const sourceUnit = wrapAndParse('let alpha')
+		const symbolTable = dependencies.getSymbolTableFor(sourceUnit)
+		const decls = <[ast.VarDecl]>symbolTable.getAllSymbolDecls()
+
+		assert(decls.length === 1)
+
+		assert(decls[0].name.rawValue === 'alpha')
+		assert(decls[0] === symbolTable.getSymbolDeclByName('alpha'))
+		assert(ast.VarDeclModifier.areCombinationsEqual(
+			decls[0].modifiers, ast.VarDeclModifier.Let
+		))
+	})
+
+	it('getAllSymbolDecls: `const` decl', () => {
+		const sourceUnit = wrapAndParse('const alpha')
+		const symbolTable = dependencies.getSymbolTableFor(sourceUnit)
+		const decls = <[ast.VarDecl]>symbolTable.getAllSymbolDecls()
+
+		assert(decls.length === 1)
+
+		assert(decls[0].name.rawValue === 'alpha')
+		assert(decls[0] === symbolTable.getSymbolDeclByName('alpha'))
+		assert(ast.VarDeclModifier.areCombinationsEqual(
+			decls[0].modifiers, ast.VarDeclModifier.Const
+		))
+	})
+
+	it('getAllSymbolDecls: multiple `let` and `const` decls', () => {
+		const sourceUnit = wrapAndParse(`
+			let a = 1
+			const b
+			let foo: Int
+			const bar: String = "abc"
+		`)
+
+		const symbolTable = dependencies.getSymbolTableFor(sourceUnit)
+		const decls = <ast.VarDecl[]>symbolTable.getAllSymbolDecls()
+
+		assert(decls.length === 4)
+
+		assert(decls[0].name.rawValue === 'a')
+		assert(decls[0] === symbolTable.getSymbolDeclByName('a'))
+		assert(ast.VarDeclModifier.areCombinationsEqual(
+			decls[0].modifiers, ast.VarDeclModifier.Let
+		))
+
+		assert(decls[1].name.rawValue === 'b')
+		assert(decls[1] === symbolTable.getSymbolDeclByName('b'))
+		assert(ast.VarDeclModifier.areCombinationsEqual(
+			decls[1].modifiers, ast.VarDeclModifier.Const
+		))
+
+		assert(decls[2].name.rawValue === 'foo')
+		assert(decls[2] === symbolTable.getSymbolDeclByName('foo'))
+		assert(ast.VarDeclModifier.areCombinationsEqual(
+			decls[2].modifiers, ast.VarDeclModifier.Let
+		))
+
+		assert(decls[3].name.rawValue === 'bar')
+		assert(decls[3] === symbolTable.getSymbolDeclByName('bar'))
+		assert(ast.VarDeclModifier.areCombinationsEqual(
+			decls[3].modifiers, ast.VarDeclModifier.Const
+		))
+	})
+
+	it('getAllSymbolDecls: `func alpha()` in SourceUnit', () => {
+		const sourceUnit = dependencies.parseSourceCodeToAST('func alpha()')
+		const symbolTable = dependencies.getSymbolTableFor(sourceUnit)
+		const decls = <[ast.FuncDecl]>symbolTable.getAllSymbolDecls()
+
+		assert(decls.length === 1)
+
+		assert(decls[0].name.rawValue === 'alpha')
+		assert(decls[0] === symbolTable.getSymbolDeclByName('alpha'))
+	})
+
+	it('getAllSymbolDecls: `class Alpha { }` in SourceUnit', () => {
+		const sourceUnit = dependencies.parseSourceCodeToAST('class Alpha { }')
+		const symbolTable = dependencies.getSymbolTableFor(sourceUnit)
+		const decls = <[ast.ClassDecl]>symbolTable.getAllSymbolDecls()
+
+		assert(decls.length === 1)
+
+		assert(decls[0].name.rawValue === 'Alpha')
+		assert(decls[0] === symbolTable.getSymbolDeclByName('Alpha'))
+	})
+
+	it('getAllSymbolDecls: ParamDeclList', () => {
+		const sourceUnit = dependencies.parseSourceCodeToAST(`
+			func alpha(a, b, foo: Int, bar) -> Void { }
+		`)
+
+		const funcDecl = <ast.FuncDecl>sourceUnit.getNodeAtIndex(0)
+		const symbolTable = dependencies.getSymbolTableFor(funcDecl.runtimeParamDecls)
+		const decls = <ast.ParamDecl[]>symbolTable.getAllSymbolDecls()
+
+		assert(decls.length === 4)
+
+		assert(decls[0].name.rawValue === 'a')
+		assert(decls[0] === symbolTable.getSymbolDeclByName('a'))
+
+		assert(decls[1].name.rawValue === 'b')
+		assert(decls[1] === symbolTable.getSymbolDeclByName('b'))
+
+		assert(decls[2].name.rawValue === 'foo')
+		assert(decls[2] === symbolTable.getSymbolDeclByName('foo'))
+
+		assert(decls[3].name.rawValue === 'bar')
+		assert(decls[3] === symbolTable.getSymbolDeclByName('bar'))
+	})
+
+	it('getAllSymbolDecls: ClassDecl members', () => {
+		const sourceUnit = dependencies.parseSourceCodeToAST(`
+			class Alpha {
+				let a = 1
+				const b
+				let foo: Int
+				const bar: String = "abc"
+			}
+		`)
+
+		const classDecl = <ast.ClassDecl>sourceUnit.getNodeAtIndex(0)
+		const symbolTable = dependencies.getSymbolTableFor(classDecl)
+		const decls = <ast.VarDecl[]>symbolTable.getAllSymbolDecls()
+
+		assert(decls.length === 4)
+
+		assert(decls[0].name.rawValue === 'a')
+		assert(decls[0] === symbolTable.getSymbolDeclByName('a'))
+		assert(ast.VarDeclModifier.areCombinationsEqual(
+			decls[0].modifiers, ast.VarDeclModifier.Let
+		))
+
+		assert(decls[1].name.rawValue === 'b')
+		assert(decls[1] === symbolTable.getSymbolDeclByName('b'))
+		assert(ast.VarDeclModifier.areCombinationsEqual(
+			decls[1].modifiers, ast.VarDeclModifier.Const
+		))
+
+		assert(decls[2].name.rawValue === 'foo')
+		assert(decls[2] === symbolTable.getSymbolDeclByName('foo'))
+		assert(ast.VarDeclModifier.areCombinationsEqual(
+			decls[2].modifiers, ast.VarDeclModifier.Let
+		))
+
+		assert(decls[3].name.rawValue === 'bar')
+		assert(decls[3] === symbolTable.getSymbolDeclByName('bar'))
+		assert(ast.VarDeclModifier.areCombinationsEqual(
+			decls[3].modifiers, ast.VarDeclModifier.Const
+		))
+	})
+
+
+	///
+	/// `SymbolTable.wasSymbolWithNameDeclared()` Tests:
+	///
+
+	it('should not return false positives for VarDecls', () => {
+		const sourceUnit = wrapAndParse(`
+			let a
+			const b
+		`)
+
+		const symbolTable = dependencies.getSymbolTableFor(sourceUnit)
+
+		assert(!symbolTable.wasSymbolWithNameDeclared('ab'))
+		assert(!symbolTable.wasSymbolWithNameDeclared('a12'))
+
+		// uppercase versions of the declared symbols should not be in the symbol table:
+		assert(!symbolTable.wasSymbolWithNameDeclared('A'))
+		assert(!symbolTable.wasSymbolWithNameDeclared('B'))
+	})
+
+	it('should not return false negatives for VarDecls', () => {
+		const sourceUnit = wrapAndParse(`
+			let a
+			const b
+		`)
+
+		const symbolTable = dependencies.getSymbolTableFor(sourceUnit)
+
+		assert(symbolTable.wasSymbolWithNameDeclared('a'))
+		assert(symbolTable.wasSymbolWithNameDeclared('b'))
+	})
+
+	it('should not return false positives for FuncDecls and ClassDecls', () => {
+		const sourceUnit = dependencies.parseSourceCodeToAST(`
+			func a() -> Void
+			class b { }
+		`)
+
+		const symbolTable = dependencies.getSymbolTableFor(sourceUnit)
+
+		assert(!symbolTable.wasSymbolWithNameDeclared('ab'))
+		assert(!symbolTable.wasSymbolWithNameDeclared('a12'))
+
+		// uppercase versions of the declared symbols should not be in the symbol table:
+		assert(!symbolTable.wasSymbolWithNameDeclared('A'))
+		assert(!symbolTable.wasSymbolWithNameDeclared('B'))
+	})
+
+	it('should not return false negatives for FuncDecls and ClassDecls', () => {
+		const sourceUnit = dependencies.parseSourceCodeToAST(`
+			func a() -> Void
+			class b { }
+		`)
+
+		const symbolTable = dependencies.getSymbolTableFor(sourceUnit)
+
+		assert(symbolTable.wasSymbolWithNameDeclared('a'))
+		assert(symbolTable.wasSymbolWithNameDeclared('b'))
 	})
 }
 
--- tests/analysis/symbolTable.ts
+++ tests/analysis/symbolTable.ts
@@ -1,5 +1,8 @@
 import { createTestSuite } from './suite/symbolTable'
 import { IScope, SymbolTable } from '@/compiler/analysis'
+import { SourceUnit } from '@/compiler/ast'
+import { parseToSourceUnit } from '@/compiler/parser'
+
 
 // tslint:disable:member-access
 
@@ -8,6 +11,16 @@ describe(
 	createTestSuite(new class {
 		getSymbolTableFor(node: IScope): SymbolTable {
 			return SymbolTable.getSymbolTableForAstNode(node)
+		}
+
+
+		private sourceUnitCount = 0
+
+		parseSourceCodeToAST(sourceCode: string): SourceUnit {
+			return parseToSourceUnit(
+				`symbol-table-test-source-unit-${++this.sourceUnitCount}`,
+				sourceCode
+			)
 		}
 	})
 )

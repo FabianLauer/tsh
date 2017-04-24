f0e498adb13f858ee59667d578f632a2b10e97d3
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2017-04-24 07:05:21 +0200

    AST: Introduce line and column numbers on `Token`

--- src/ast/Token.ts
+++ src/ast/Token.ts
@@ -6,7 +6,27 @@ export class Token extends BaseNode {
 		/**
 		 * The raw value of this token in source code.
 		 */
-		public readonly rawValue: string
+		public readonly rawValue: string,
+
+		/**
+		 * The start line number of the token in source code.
+		 */
+		public readonly startLineNumber: number = undefined,
+
+		/**
+		 * The start column number of the token in source code.
+		 */
+		public readonly startColumnNumber: number = undefined,
+
+		/**
+		 * The end line number of the token in source code.
+		 */
+		public readonly endLineNumber: number = undefined,
+
+		/**
+		 * The end column number of the token in source code.
+		 */
+		public readonly endColumnNumber: number = undefined
 	) {
 		super()
 		assertAstNodeParam(typeof rawValue === 'string')

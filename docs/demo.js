6bb37254f55c46e4f07a607ad227fb59296c9d10
Fabian Lauer <FabianLauer@users.noreply.github.com>
2019-03-25 15:47:24 +0100

    Demo: Run code, add output panel

--- docs/demo.js
+++ docs/demo.js
@@ -47,15 +47,30 @@ require(window.createJSArray('vs/editor/editor.main'), (function () {
 		function startDemo(monaco) {
 			var sourceEditor = createEditor(monaco, "source-container", "swift", false, getSampleCode());
 var compiledEditor = createEditor(monaco, "compiled-container", "javascript", true, "");
-var recompile = (function () {
-			compile(sourceEditor, compiledEditor);
+var lastRun = 0;
+var lastRunTimeout;
+var recompileAndRun = (function () {
+			var result = compile(sourceEditor, compiledEditor);
+clearTimeout(lastRunTimeout);
+if (result == CompileResult.CompilationSuccessful) {
+			var now = Date.now();
+if (now - lastRun < 100) {
+			lastRunTimeout = setTimeout(recompileAndRun);
+
+		}
+else {
+			run(compiledEditor.getValue());
+
+		}
+
+		}
 
 		});
-recompile();
-sourceEditor.onDidType(recompile);
-sourceEditor.onDidPaste(recompile);
-sourceEditor.onDidBlurEditor(recompile);
-sourceEditor.onDidChangeModelContent(recompile);
+recompileAndRun();
+sourceEditor.onDidType(recompileAndRun);
+sourceEditor.onDidPaste(recompileAndRun);
+sourceEditor.onDidBlurEditor(recompileAndRun);
+sourceEditor.onDidChangeModelContent(recompileAndRun);
 
 		}
 		
@@ -102,18 +117,27 @@ return monaco.editor.create(document.getElementById(containerId), options);
 		};
 
 
+		/** @enum CompileResult */
+		var CompileResult = {
+			'CompilationSuccessful': 'CompilationSuccessful',
+'CompilationFailed': 'CompilationFailed'
+		};
+
+
 		function compile(sourceEditor, compiledEditor) {
-			clearCompileProblems();
+			clearProblems();
 var sourceCode = sourceEditor.getValue();
 var code = "";
 var compiled = false;
 var compiledCode;
+var result = CompileResult.CompilationSuccessful;
 try {
 			compiledCode = compileToTarget(sourceCode, CompileTarget.JavaScript);
 compiled = true;
 
 		} catch (err) {
-			renderCompileProblem(err);
+			result = CompileResult.CompilationFailed;
+renderProblem(ProblemType.CompileTime, err);
 
 		}if (compiled) {
 			code += compiledCode;
@@ -125,6 +149,7 @@ compiled = true;
 
 		} catch {
 			compiled = false;
+result = CompileResult.CompilationFailed;
 
 		}if (compiled) {
 			compiledCode = compiledCode.replace(_new(RegExp, "\t+", "g"), "  ");
@@ -133,7 +158,23 @@ code += "/*\n\n" + compiledCode + "\n*/";
 
 		}
 compiledEditor.setValue(tryBeautifyJavaScript(code));
+return result;
 
+		}
+		
+		function run(code) {
+			clearOutput();
+js = "(function(exports, print, require) {";
+js += code;
+js += "; main();";
+js += "})({}, window.renderOutput, function() { throw new Error('import/reuqire not supported.') })";
+try {
+			window.runJavaScript(js);
+
+		} catch (runtimeErr) {
+			renderProblem(ProblemType.RunTime, runtimeErr);
+
+		}
 		}
 		
 		function compileToTarget(sourceCode, compileTarget) {
@@ -161,12 +202,19 @@ return api.compileSourceCode(sourceCode, target);
 		}
 		}
 		
+		/** @enum ProblemType */
+		var ProblemType = {
+			'CompileTime': 'CompileTime',
+'RunTime': 'RunTime'
+		};
+
+
 		function getProblemsPanelContentElement() {
 			return document.getElementById("problems-panel-content");
 
 		}
 		
-		function clearCompileProblems() {
+		function clearProblems() {
 			var problemsPanelContent = getProblemsPanelContentElement();
 problemsPanelContent.innerHTML = "";
 var content = document.createElement("span");
@@ -175,12 +223,39 @@ problemsPanelContent.appendChild(content);
 
 		}
 		
-		function renderCompileProblem(error) {
+		function renderProblem(type, error) {
 			var problemsPanelContent = getProblemsPanelContentElement();
 problemsPanelContent.innerHTML = "";
 var problem = document.createElement("pre");
-problem.innerText = error.message;
+if (type == ProblemType.CompileTime) {
+			problem.innerText = "Compiler Error:";
+
+		}
+else {
+			problem.innerText = "Runtime Error:";
+
+		}
+problem.innerText += "\n" + error.message;
 problemsPanelContent.appendChild(problem);
+console.warn("Problem:", type, error);
+
+		}
+		
+		function getOutputPanelContentElement() {
+			return document.getElementById("output-panel-content");
+
+		}
+		
+		function clearOutput() {
+			var outputPanelContent = getOutputPanelContentElement();
+outputPanelContent.innerHTML = "";
+
+		}
+		
+		function renderOutput(output) {
+			var outputPanelContent = getOutputPanelContentElement();
+outputPanelContent.innerHTML += output;
+console.log("Output:", output);
 
 		}
 		
